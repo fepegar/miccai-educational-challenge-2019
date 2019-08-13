c96777d654ac577c0dba218038f76c2497de946a
Fernando Perez-Garcia <fernando.perezgarcia.17@ucl.ac.uk>
2019-08-13 14:16:51 +0100

    Add custom mapping module

---
+++ highresnet_mapping.py
@@ -0,0 +1,67 @@
+import re
+
+
+def is_not_valid(variable_name, shape):
+    exclusion_criteria = (
+        'Adam' in variable_name,  # used for training
+        'biased' in variable_name,  # unused
+        not shape,  # empty variables
+        'ExponentialMovingAverage' in variable_name,  # unused by NiftyNet model zoo
+    )
+    return any(exclusion_criteria)
+
+
+def tf2pt_name(name_tf):
+    """
+    Return the equivalent PyTorch parameter name of the TensorFlow
+    variable. Rules have been created from visual inspection of the
+    variables lists.
+    """
+    param_type_dict = {
+        'w': 'weight',
+        'gamma': 'weight',
+        'beta': 'bias',
+        'moving_mean': 'running_mean',
+        'moving_variance': 'running_var',
+    }
+
+    if name_tf.startswith('res_'):
+        # For example: 'res_2_0/bn_0/moving_variance'
+        pattern = (
+            'res'
+            r'_(\d)'   # dil_idx = 2
+            r'_(\d)'   # res_idx = 0
+            r'/(\w+)'  # layer_type = bn
+            r'_(\d)'   # layer_idx = 0
+            r'/(\w+)'  # param_type = moving_variance
+        )
+        groups = re.match(pattern, name_tf).groups()
+        dil_idx, res_idx, layer_type, layer_idx, param_type = groups
+        param_idx = 3 if layer_type == 'conv' else 0
+
+        name_pt = (
+            f'block.{dil_idx}.dilation_block.{res_idx}.residual_block'
+            f'.{layer_idx}.convolutional_block.{param_idx}.{param_type_dict[param_type]}'
+        )
+    elif name_tf.startswith('conv_'):
+        conv_layers_dict = {
+            'conv_0_bn_relu/conv_/w': 'block.0.convolutional_block.1.weight',  # first conv layer
+            'conv_0_bn_relu/bn_/gamma': 'block.0.convolutional_block.2.weight',
+            'conv_0_bn_relu/bn_/beta': 'block.0.convolutional_block.2.bias',
+            'conv_0_bn_relu/bn_/moving_mean': 'block.0.convolutional_block.2.running_mean',
+            'conv_0_bn_relu/bn_/moving_variance': 'block.0.convolutional_block.2.running_var',
+
+            'conv_1_bn_relu/conv_/w': 'block.4.convolutional_block.0.weight',  # layer with dropout
+            'conv_1_bn_relu/bn_/gamma': 'block.4.convolutional_block.1.weight',
+            'conv_1_bn_relu/bn_/beta': 'block.4.convolutional_block.1.bias',
+            'conv_1_bn_relu/bn_/moving_mean': 'block.4.convolutional_block.1.running_mean',
+            'conv_1_bn_relu/bn_/moving_variance': 'block.4.convolutional_block.1.running_var',
+
+            'conv_2_bn/conv_/w': 'block.6.convolutional_block.0.weight',  # classifier
+            'conv_2_bn/bn_/gamma': 'block.6.convolutional_block.1.weight',
+            'conv_2_bn/bn_/beta': 'block.6.convolutional_block.1.bias',
+            'conv_2_bn/bn_/moving_mean': 'block.6.convolutional_block.1.running_mean',
+            'conv_2_bn/bn_/moving_variance': 'block.6.convolutional_block.1.running_var',
+        }
+        name_pt = conv_layers_dict[name_tf]
+    return name_pt
--- tf2pt.py
+++ tf2pt.py
@@ -1,4 +1,3 @@
-import re
 from collections import OrderedDict
 
 import torch
@@ -38,7 +37,7 @@ def transpose_to_pytorch(array):
     array = np.transpose(array, shape)
     return array
 
-    
+
 def transpose_to_tensorflow(array):
     """
     See docs of tf.nn.conv3d
@@ -72,64 +71,8 @@ def torch_logits_to_niftynet_labels(logits):
     return labels
 
 
-def tf2pt_name(name_tf):
-    """
-    Return the equivalent PyTorch parameter name of the TensorFlow
-    variable. Rules have been created from visual inspection of the
-    variables lists.
-    """
-    param_type_dict = {
-        'w': 'weight',
-        'gamma': 'weight',
-        'beta': 'bias',
-        'moving_mean': 'running_mean',
-        'moving_variance': 'running_var',
-    }
-    
-    if name_tf.startswith('res_'):
-        # For example: 'res_2_0/bn_0/moving_variance'
-        pattern = (
-            'res'
-            r'_(\d)'  # 2 dil_idx
-            r'_(\d)'  # 0 res_idx
-            r'/(\w+)' # bn layer_type
-            r'_(\d)'  # 0 layer_idx
-            r'/(\w+)'  # moving_variance param_type
-        )
-        groups = re.match(pattern, name_tf).groups()
-        dil_idx, res_idx, layer_type, layer_idx, param_type = groups
-        param_idx = 3 if layer_type == 'conv' else 0
-            
-        name_pt = (
-            f'block.{dil_idx}.dilation_block.{res_idx}.residual_block'
-            f'.{layer_idx}.convolutional_block.{param_idx}.{param_type_dict[param_type]}'
-        )
-    elif name_tf.startswith('conv_'):
-        conv_layers_dict = {
-            'conv_0_bn_relu/conv_/w': 'block.0.convolutional_block.1.weight',  # first conv layer
-            'conv_0_bn_relu/bn_/gamma': 'block.0.convolutional_block.2.weight',  
-            'conv_0_bn_relu/bn_/beta': 'block.0.convolutional_block.2.bias',
-            'conv_0_bn_relu/bn_/moving_mean': 'block.0.convolutional_block.2.running_mean',
-            'conv_0_bn_relu/bn_/moving_variance': 'block.0.convolutional_block.2.running_var',
-
-            'conv_1_bn_relu/conv_/w': 'block.4.convolutional_block.0.weight',  # layer with dropout
-            'conv_1_bn_relu/bn_/gamma': 'block.4.convolutional_block.1.weight',  
-            'conv_1_bn_relu/bn_/beta': 'block.4.convolutional_block.1.bias',
-            'conv_1_bn_relu/bn_/moving_mean': 'block.4.convolutional_block.1.running_mean',
-            'conv_1_bn_relu/bn_/moving_variance': 'block.4.convolutional_block.1.running_var',
-
-            'conv_2_bn/conv_/w': 'block.6.convolutional_block.0.weight',  # classifier
-            'conv_2_bn/bn_/gamma': 'block.6.convolutional_block.1.weight',  
-            'conv_2_bn/bn_/beta': 'block.6.convolutional_block.1.bias',
-            'conv_2_bn/bn_/moving_mean': 'block.6.convolutional_block.1.running_mean',
-            'conv_2_bn/bn_/moving_variance': 'block.6.convolutional_block.1.running_var',
-        }
-        name_pt = conv_layers_dict[name_tf]
-    return name_pt
-    
-    
-def tf2pt(name_tf, tensor_tf):
-    name_pt = tf2pt_name(name_tf)
+def tf2pt(name_tf, tensor_tf, name_mapping_function):
+    name_pt = name_mapping_function(name_tf)
     num_dimensions = tensor_tf.dim()
     if num_dimensions == 1:
         tensor_pt = tensor_tf
@@ -138,61 +81,70 @@ def tf2pt(name_tf, tensor_tf):
     return name_pt, tensor_pt
 
 
-def is_not_valid(variable_name, shape):
-    exclusion_criteria = (
-        'Adam' in variable_name,  # used for training
-        'biased' in variable_name,  # unused
-        not shape,  # empty variables
-        'ExponentialMovingAverage' in variable_name,  # unused by NiftyNet model zoo
-    )
-    return any(exclusion_criteria)
-
-
 """
-This last couple of functions are a good remainder to myself that
+This last couple of functions are a good reminder to myself that
 TensorFlow makes me sad and PyTorch makes me happy
 """
-def checkpoint_to_state_dict_(checkpoint_path, csv_path, state_dict_path, filter_variables=True):
+
+
+def checkpoint_tf_to_state_dict_tf_(
+        input_checkpoint_tf_path,
+        output_csv_tf_path,
+        output_state_dict_tf_path,
+        filter_out_function=None,
+        replace_string=None,
+        ):
     tf.reset_default_graph()
 
     rows = []
     variables_dict = OrderedDict()
-    variables_list = tf.train.list_variables(str(checkpoint_path))
+    variables_list = tf.train.list_variables(str(input_checkpoint_tf_path))
     for name, shape in variables_list:
-        if filter_variables and is_not_valid(name, shape):
+        if filter_out_function is not None and filter_out_function(name, shape):
             continue
         variables_dict[name] = tf.get_variable(name, shape=shape)
-        name = name.replace('HighRes3DNet/', '')
+        if replace_string is not None:
+            name = name.replace(replace_string, '')
         shape = ', '.join(str(n) for n in shape)
         row = {'name': name, 'shape': shape}
         rows.append(row)
     data_frame = pd.DataFrame.from_dict(rows)
-    
+
     saver = tf.train.Saver()
     state_dict = {}
     with tf.Session() as sess:
-        saver.restore(sess, str(checkpoint_path))
+        saver.restore(sess, str(input_checkpoint_tf_path))
         for name, shape in variables_list:
-            if filter_variables and is_not_valid(name, shape):
+            if (
+                    filter_out_function is not None
+                    and filter_out_function(name, shape)
+                    ):
                 continue
             array = variables_dict[name].eval()
-            name = name.replace('HighRes3DNet/', '')
+            if replace_string is not None:
+                name = name.replace(replace_string, '')
             state_dict[name] = torch.tensor(array)
-    
-    data_frame.to_csv(csv_path)
-    torch.save(state_dict, state_dict_path)
+
+    data_frame.to_csv(output_csv_tf_path)
+    torch.save(state_dict, output_state_dict_tf_path)
 
 
-def checkpoint_to_state_dict(*args, **kwargs):
+def checkpoint_tf_to_state_dict_tf(*args, **kwargs):
     """
+    This is done so that the GPU can be used by PyTorch afterwards
     https://stackoverflow.com/a/44842044/3956024
+
+    If you don't need to run the inference, call
+    checkpoint_tf_to_state_dict_tf_ instead to avoid potential headaches
+
+    This might break things on Windows if trying to run as a Python file.
+    See https://github.com/pytorch/pytorch/issues/5858#issuecomment-373950687
     """
     import multiprocessing
     p = multiprocessing.Process(
-        target=checkpoint_to_state_dict_,
+        target=checkpoint_tf_to_state_dict_tf_,
         args=args,
         kwargs=kwargs,
     )
     p.start()
     p.join()
-    
